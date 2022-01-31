562b9c4b18ce38d8dacf48b267071b3247d89db4
mohtashimKdfd <mohtashim.kamran@unthinkable.co>
2022-02-01 01:03:20 +0530

    Create Read Delete Job

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template, url_for
+from flask import Flask, redirect , render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -16,10 +16,32 @@ class User(db.Model):
     def __repr__(self) -> str:
         return '<User %r>' % self.name
 
-@app.route('/')
+@app.route('/',methods=['GET', 'POST'])
 def index():
-    # return 'Hello World'
-    return render_template('index.html')
+    if request.method == 'POST':
+        username = request.form['name']
+        newUser = User(name=username)
+        try:
+            db.session.add(newUser)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "There was an issue creating the user"
+    else:
+        allUsers = User.query.order_by('date_created').all()
+        return render_template('index.html',users=allUsers)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = User.query.get_or_404(id)
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except Exception as e:
+        print(e)
+        return "Invalid request"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
